e1cc4d53b088aff7d65b304dabf1926eea25bced
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-04 15:01:49 +0600

    feat[added pagination]

--- pages/films.js
+++ pages/films.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { fetcher } from "../lib/api";
+import Pagination from "@mui/material/Pagination";
+import useSWR from "swr";
 
 function FilmsList({ films }) {
-  console.log("films:", films);
+  const [page, setPage] = React.useState(1);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const { data } = useSWR(
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/films?pagination[page]=${page}&pagination[pageSize]=2`,
+    fetcher,
+    { fallbackData: films }
+  );
+  console.log("data:", data);
+
   return (
     <Layout>
       <p>FilmsList</p>
-      {films.data.map((film) => (
+      {data.data.map((film) => (
         <div key={film.attributes.id} className="my-3 bg-slate-100 p-2">
           <h1>Title: {film.attributes.title}</h1>
           <p className="text-sm font-normal">
@@ -18,13 +32,20 @@ function FilmsList({ films }) {
           </p>
         </div>
       ))}
+      <div>
+        <Pagination
+          count={data?.meta?.pagination?.pageCount}
+          page={page}
+          onChange={handleChange}
+        />
+      </div>
     </Layout>
   );
 }
 
 export async function getStaticProps() {
   const filmsResponse = await fetcher(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/films`
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/films?pagination[page]=1&pagination[pageSize]=2`
   );
   console.log("filmsResponse: ", filmsResponse);
   return {
