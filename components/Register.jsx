a694a7f37a1d8911659a09436b67e79f037d4518
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-08 11:22:11 +0600

    feat[working with register]

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,7 +1,142 @@
-import React from "react";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import { fetcher } from "../lib/api";
+import { setToken } from "../lib/auth";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 function Register() {
-  return <div>Register</div>;
-}
+  const router = useRouter();
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    showPassword: false,
+  });
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const responseData = await fetcher(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/auth/local/register`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: userData.email,
+            password: userData.password,
+            username: userData.username,
+          }),
+          method: "POST",
+        }
+      );
+      setToken(responseData);
+      router.redirect("/profile");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  console.log("userData:", userData);
+
+  const handleClickShowPassword = () => {
+    setUserData({
+      ...userData,
+      showPassword: !userData.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form
+        action=""
+        className="m-3 flex gap-3 flex-col"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          required
+          fullWidth
+          size="small"
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          name="username"
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          fullWidth
+          size="small"
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          type="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <FormControl variant="outlined" size="small" fullWidth>
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            required
+            fullWidth
+            size="small"
+            id="outlined-adornment-password"
+            type={userData.showPassword ? "text" : "password"}
+            value={userData.password}
+            name="password"
+            onChange={handleChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {userData.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <Button
+          size="small"
+          type="submit"
+          variant="contained"
+          sx={{
+            color: "black",
+            "&:hover": {
+              color: "white",
+            },
+          }}
+        >
+          Register
+        </Button>
+      </form>
+    </div>
+  );
+}
 export default Register;
